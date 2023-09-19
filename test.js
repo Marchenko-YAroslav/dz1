8654245f2e4865fadaae296c864e913c76286d75
Yaroslav <skeletony126@gmail.com>
2023-09-19 19:59:02 +0300

    dz1

---
+++ test.js
@@ -0,0 +1,6 @@
+let name = "Yaroslav"
+let surename = 'Marchenko'
+let age = 17
+let hobby = 'play active games'
+
+console.log('Мене звати ' + name +' ' + surename + ' менi ' + age + ' рокiв ' + ', у вiльный час я полюбляю ' + hobby )
